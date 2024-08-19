4e814a167bb5c56d671e11f10c72e8c1575b8b04
jasphia <jasphia.exe@gmail.com>
2024-08-18 19:36:14 -0500

    update test

--- hello.py
+++ hello.py
@@ -3,17 +3,20 @@
 # Enter your code on line 7 to print 'Hello, World!'
 # to the console (the panel on the right)
 
-print("Hello");
+print("Hello, World!");
 
 # Next, PLAY!
 # Modify your code on line 7 to try to accomplish the tasks listed below:
-
+print("hello friend")
 #   1. Change the message that is printed.
 #   2. Figure out what the parentheses do. Will the code work without them?
 #   3. Remove one or both quotation marks. Do we need to include both opening and closing quote marks? Is there a difference between using a single or a double quote (' vs. ")?
 #   4. Print a number. (Bonus: Print two numbers added together).
+print(2 + 6)
 #   5. Print multiple messages one after the other.
 #   6. Print two messages on the same line.
+print("two msgs", "on same line")
 #   7. Print a message that contains quote marks, such as Quoth the Raven "Nevermore".
+print('mgr with "quotes"')
 #   8. Other. You choose!
-#
+#py
